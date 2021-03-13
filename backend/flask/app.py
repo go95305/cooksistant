2eac0b58987024fe9e4018fc2cacee3b11c9b017
KYC <go95305@naver.com>
2021-03-13 13:38:01 +0900

    Add Flask Server Flask기본 설정 업데이트

---
+++ backend/flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+app = Flask(__name__)
+api = Api(app)
+ns = api.namespace('TEST', description='레시피 추천 기능 API')
+
+
+@ns.route('/hello/<string:name>')
+class HelloWorld(Resource):
+    def get(self,name):
+         return {"message": "welcome, %s!" % name}
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=80)
