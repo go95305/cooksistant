760d467c935bede0f83ef9649b64bc49850a2c9a
KYC <go95305@naver.com>
2021-03-15 11:09:36 +0900

    Create branch backend/restserver spring으로 rest 서버를 구축

---
+++ Rest_server/src/main/java/com/project/cooksistant/CooksistantApplication.java
@@ -0,0 +1,13 @@
+package com.project.cooksistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooksistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooksistantApplication.class, args);
+	}
+
+}
