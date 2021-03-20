b140d0b9a1c93d3bdc764113f0dca64c7ad2ffc5
김부희 <kimbuhui@naver.com>
2021-03-20 21:33:28 +0900

    평가한 레시피인지 아닌지 분류

--- frontend/components/EvalueCard.js
+++ frontend/components/EvalueCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
 import { Block, Text, theme } from 'galio-framework';
 
-import { nowTheme } from '../constants';
+import { Images as check, nowTheme } from '../constants';
 
 class Card extends React.Component {
   render() {
@@ -14,35 +14,40 @@ class Card extends React.Component {
       horizontal,
       full,
       style,
-      ctaColor,
+      isEvaluColor,
       imageStyle,
-      ctaRight,
-      titleStyle
+      titleStyle,
     } = this.props;
 
     const imageStyles = [full ? styles.fullImage : styles.horizontalImage, imageStyle];
-    const titleStyles = [styles.cardTitle, titleStyle];
     const cardContainer = [styles.card, styles.shadow, style];
     const imgContainer = [
       styles.imageContainer,
       horizontal ? styles.horizontalStyles : styles.verticalStyles,
-      styles.shadow
+      styles.shadow,
     ];
 
     return (
       <Block row={horizontal} card flex style={cardContainer}>
-        <TouchableWithoutFeedback onPress={() => navigation.navigate('Profile')}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            item.isEvalu ? navigation.navigate('Pro') : navigation.navigate('EvalueRegister');
+          }}
+        >
           <Block flex style={imgContainer}>
             <Image resizeMode="cover" source={item.image} style={imageStyles} />
           </Block>
         </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback onPress={() => navigation.navigate('Profile')}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            item.isEvalu ? navigation.navigate('Pro') : navigation.navigate('EvalueRegister');
+          }}
+        >
           <Block flex space="between" style={styles.cardDescription}>
             <Block flex>
               <Text
-                style={{ fontFamily: 'montserrat-regular' }}
-                size={14}
-                style={titleStyles}
+                style={{ fontFamily: 'montserrat-bold', paddingTop: 15, paddingHorizontal: 10 }}
+                size={15}
                 color={nowTheme.COLORS.SECONDARY}
               >
                 {item.title}
@@ -50,53 +55,32 @@ class Card extends React.Component {
               {item.subtitle ? (
                 <Block flex center>
                   <Text
-                    style={{ fontFamily: 'montserrat-regular' }}
-                    size={32}
+                    style={{ fontFamily: 'montserrat-bold' }}
+                    size={10}
                     color={nowTheme.COLORS.BLACK}
                   >
                     {item.subtitle}
                   </Text>
                 </Block>
               ) : (
-                  <Block />
-                )}
-              {item.description ? (
-                <Block flex center>
-                  <Text
-                    style={{ fontFamily: 'montserrat-regular', textAlign: 'center', padding: 15 }}
-                    size={14}
-                    color={"#9A9A9A"}
-                  >
-                    {item.description}
-                  </Text>
-                </Block>
-              ) : (
-                  <Block />
-                )}
-              {item.body ? (
-                <Block flex left>
-                  <Text
-                    style={{ fontFamily: 'montserrat-regular' }}
-                    size={12}
-                    color={nowTheme.COLORS.TEXT}
-                  >
-                    {item.body}
-                  </Text>
-                </Block>
-              ) : (
-                  <Block />
-                )}
+                <Block />
+              )}
             </Block>
-            <Block right={ctaRight ? true : false}>
+            <Block row space="between">
               <Text
-                style={styles.articleButton}
-                size={12}
-                muted={!ctaColor}
-                color={ctaColor || nowTheme.COLORS.ACTIVE}
+                style={{ paddingHorizontal: 10, paddingBottom: 5 }}
+                size={13}
+                muted={!isEvaluColor}
+                color={'#f18d46'}
                 bold
               >
-                {item.cta}
+                {item.isEvalu ? '레시피 보러가기' : '레시피 평가하기'}
               </Text>
+              {item.isEvalu ? (
+                <Image source={check.isEvalue} style={{ marginRight: 8 }} />
+              ) : (
+                <Image source={check.etc} style={{ marginRight: 8 }} />
+              )}
             </Block>
           </Block>
         </TouchableWithoutFeedback>
@@ -109,11 +93,11 @@ Card.propTypes = {
   item: PropTypes.object,
   horizontal: PropTypes.bool,
   full: PropTypes.bool,
-  ctaColor: PropTypes.string,
+  isEvaluColor: PropTypes.string,
   imageStyle: PropTypes.any,
-  ctaRight: PropTypes.bool,
+  isEvaluRight: PropTypes.bool,
   titleStyle: PropTypes.any,
-  textBodyStyle: PropTypes.any
+  textBodyStyle: PropTypes.any,
 };
 
 const styles = StyleSheet.create({
@@ -122,51 +106,51 @@ const styles = StyleSheet.create({
     marginVertical: theme.SIZES.BASE,
     borderWidth: 0,
     minHeight: 114,
-    marginBottom: 4
+    marginBottom: 4,
   },
   cardTitle: {
     paddingHorizontal: 9,
     paddingTop: 7,
-    paddingBottom: 15
+    paddingBottom: 15,
   },
   cardDescription: {
-    padding: theme.SIZES.BASE / 2
+    padding: theme.SIZES.BASE / 2,
   },
   imageContainer: {
     borderRadius: 3,
     elevation: 1,
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   image: {
     // borderRadius: 3,
   },
   horizontalImage: {
     height: 122,
-    width: 'auto'
+    width: 'auto',
   },
   horizontalStyles: {
     borderTopRightRadius: 0,
-    borderBottomRightRadius: 0
+    borderBottomRightRadius: 0,
   },
   verticalStyles: {
     borderBottomRightRadius: 0,
-    borderBottomLeftRadius: 0
+    borderBottomLeftRadius: 0,
   },
   fullImage: {
-    height: 215
+    height: 215,
   },
   shadow: {
     shadowColor: '#8898AA',
     shadowOffset: { width: 0, height: 1 },
     shadowRadius: 6,
     shadowOpacity: 0.1,
-    elevation: 2
+    elevation: 2,
   },
   articleButton: {
     fontFamily: 'montserrat-bold',
     paddingHorizontal: 9,
-    paddingVertical: 7
-  }
+    paddingVertical: 7,
+  },
 });
 
 export default withNavigation(Card);
