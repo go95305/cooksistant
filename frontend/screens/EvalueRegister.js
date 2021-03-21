790663b23d6d280300ca7b77489a7eff6fee5745
김부희 <kimbuhui@naver.com>
2021-03-21 23:50:41 +0900

    Update: 레시피 평가 등록 UI

--- frontend/screens/EvalueRegister.js
+++ frontend/screens/EvalueRegister.js
@@ -7,6 +7,7 @@ import {
   StatusBar,
   TouchableWithoutFeedback,
 } from 'react-native';
+import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
 import SwitchSelector from 'react-native-switch-selector';
 import { Block, Checkbox, Text, Button as GaButton, theme } from 'galio-framework';
 
@@ -21,7 +22,12 @@ const recipe = {
   isEvalu: false,
 };
 
-const options = [
+const isLike = [
+  { label: '좋아요', value: 0 },
+  { label: '싫어요', value: 1 },
+];
+
+const isTaste = [
   { label: '1', value: 1 },
   { label: '2', value: 2 },
   { label: '3', value: 3 },
@@ -29,10 +35,45 @@ const options = [
   { label: '5', value: 5 },
 ];
 
-const result = [];
-
 class TasteRegister extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  defaultScrollViewProps = {
+    scrollEnabled: false,
+    contentContainerStyle: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+  };
+
+  onPrevStep = () => {
+    console.log('called previous step');
+  };
+  onNextStep = () => {
+    console.log('called next step');
+  };
+  onSubmitSteps = () => {
+    console.log('called on submit step.');
+  };
+
   render() {
+    const progressStepsStyle = {
+      labelFontFamily: 'montserrat-bold',
+      activeStepIconBorderColor: '#f18d46',
+      activeLabelColor: '#f18d46',
+      activeStepNumColor: '#f18d46',
+      completedStepIconColor: '#f18d46',
+      completedProgressBarColor: '#f18d46',
+    };
+
+    const buttonTextStyle = {
+      color: '#f18d46',
+      fontFamily: 'montserrat-bold',
+      fontSize: 17,
+    };
+
     return (
       <Block style={styles.container}>
         <Block flex middle>
@@ -43,66 +84,200 @@ class TasteRegister extends React.Component {
           >
             <Block flex={1} middle>
               <Block style={styles.registerContainer}>
-                  <Block>
-                    <Text
-                      style={{
-                        fontFamily: 'montserrat-regular',
-                        textAlign: 'center',
-                        marginTop: Platform.OS === 'android' ? 25 : 20,
-                        marginBottom: 10,
-                      }}
-                      color="#333"
-                      size={19}
-                    >
-                      레시피의 맛은 어땠나요?
-                    </Text>
-                  </Block>
-                  <Block
-                    center
-                    style={{
-                      borderColor: '#f2f2f2',
-                      width: '85%',
-                      marginBottom: 10,
-                      borderWidth: StyleSheet.hairlineWidth,
-                    }}
-                  />
-                  <Block center>
-                    <Image
-                      source={recipe.image}
-                      style={{
-                        width: width - theme.SIZES.BASE * 7,
-                        height: 180,
-                      }}
-                    />
-                    <Text
-                      style={{
-                        fontFamily: 'montserrat-bold',
-                        textAlign: 'center',
-                        marginTop: 15,
-                        marginBottom: 15,
-                      }}
-                      color="#333"
-                      size={19}
-                    >
-                      {recipe.title}
-                    </Text>
-                  </Block>
-                  <Block>
-                      
-                  </Block>
-                  <Block flex={1} center >
-                      <Button color="primary" round style={styles.createButton}>
+                <ProgressSteps {...progressStepsStyle}>
+                  <ProgressStep
+                    label="좋아요"
+                    onNext={this.onNextStep}
+                    scrollViewProps={this.defaultScrollViewProps}
+                    nextBtnTextStyle={buttonTextStyle}
+                    previousBtnTextStyle={buttonTextStyle}
+                  >
+                    <Block flex={1} style={{ alignItems: 'center' }}>
+                      <Block style={{ marginTop: 10 }}>
                         <Text
-                          style={{ fontFamily: 'montserrat-bold' }}
-                          size={14}
-                          color={nowTheme.COLORS.WHITE}
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginTop: 10,
+                            marginBottom: 15,
+                          }}
+                          color="#333"
+                          size={19}
                         >
-                          다음 단계
+                          {recipe.title}
                         </Text>
-                      </Button>
-                  </Block>
-                </Block>
+                        <Image
+                          source={recipe.image}
+                          style={{
+                            width: width - theme.SIZES.BASE * 6,
+                            height: 200,
+                          }}
+                        />
+                      </Block>
+                      <Block flex={1} center style={styles.likeContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 20 : 15,
+                          }}
+                          color="#333"
+                          size={15}
+                        >
+                          레시피가 마음에 들었나요?
+                        </Text>
+                        <SwitchSelector
+                          options={isLike}
+                          initial={1}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          textStyle={{ fontFamily: 'montserrat-regular' }}
+                          selectedTextStyle={{ fontFamily: 'montserrat-regular' }}
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => {
+                            console.log(`Call onPress with value: ${value}`);
+                          }}
+                        />
+                      </Block>
+                    </Block>
+                  </ProgressStep>
+                  <ProgressStep
+                    label="맛"
+                    onPrevious={this.onPrevStep}
+                    onSubmit={this.onSubmitSteps}
+                    scrollViewProps={this.defaultScrollViewProps}
+                    nextBtnTextStyle={buttonTextStyle}
+                    previousBtnTextStyle={buttonTextStyle}
+                  >
+                    <Block>
+                      <Block center style={styles.tasteContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 10 : 15,
+                          }}
+                          color="#333"
+                          size={13}
+                        >
+                          매운 맛
+                        </Text>
+                        <SwitchSelector
+                          options={isTaste}
+                          initial={2}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => console.log(`Call onPress with value: ${value}`)}
+                        />
+                      </Block>
+                      <Block center style={styles.tasteContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 10 : 15,
+                          }}
+                          color="#333"
+                          size={13}
+                        >
+                          짠 맛
+                        </Text>
+                        <SwitchSelector
+                          options={isTaste}
+                          initial={2}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => console.log(`Call onPress with value: ${value}`)}
+                        />
+                      </Block>
+                      <Block center style={styles.tasteContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 10 : 15,
+                          }}
+                          color="#333"
+                          size={13}
+                        >
+                          단 맛
+                        </Text>
+                        <SwitchSelector
+                          options={isTaste}
+                          initial={2}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => console.log(`Call onPress with value: ${value}`)}
+                        />
+                      </Block>
+                      <Block center style={styles.tasteContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 10 : 15,
+                          }}
+                          color="#333"
+                          size={13}
+                        >
+                          신 맛
+                        </Text>
+                        <SwitchSelector
+                          options={isTaste}
+                          initial={2}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => console.log(`Call onPress with value: ${value}`)}
+                        />
+                      </Block>
+                      <Block center style={styles.tasteContainer}>
+                        <Text
+                          style={{
+                            fontFamily: 'montserrat-bold',
+                            textAlign: 'center',
+                            marginBottom: Platform.OS === 'android' ? 10 : 15,
+                          }}
+                          color="#333"
+                          size={13}
+                        >
+                          쓴 맛
+                        </Text>
+                        <SwitchSelector
+                          options={isTaste}
+                          initial={2}
+                          fontSize={15}
+                          textColor="#f18d46"
+                          selectedColor="white"
+                          buttonColor="#f18d46"
+                          borderColor="#f18d46"
+                          hasPadding
+                          onPress={(value) => console.log(`Call onPress with value: ${value}`)}
+                        />
+                      </Block>
+                    </Block>
+                  </ProgressStep>
+                </ProgressSteps>
               </Block>
+            </Block>
           </ImageBackground>
         </Block>
       </Block>
@@ -127,7 +302,7 @@ const styles = StyleSheet.create({
   registerContainer: {
     marginTop: 55,
     width: width * 0.9,
-    height: height < 812 ? height * 0.8 : height * 0.8,
+    height: height * 0.8,
     backgroundColor: nowTheme.COLORS.WHITE,
     borderRadius: 4,
     shadowColor: nowTheme.COLORS.BLACK,
@@ -142,10 +317,11 @@ const styles = StyleSheet.create({
   },
   tasteContainer: {
     width: width * 0.7,
-    marginBottom: Platform.OS === 'android' ? 15 : 20,
+    marginBottom: 10,
   },
-  createButton: {
-    width: width * 0.5,
+  likeContainer: {
+    width: width * 0.7,
+    marginTop: Platform.OS === 'android' ? 30 : 40,
   },
 });
 
