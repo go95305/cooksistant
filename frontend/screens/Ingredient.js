27b6e51aec9078d41132fc0f5fac0620c2306379
KimHaneul <lldkdlzb@naver.com>
2021-03-25 21:32:59 +0900

    [fix] imagegallery

--- frontend/screens/Ingredient.js
+++ frontend/screens/Ingredient.js
@@ -10,12 +10,6 @@ import {
 } from 'react-native';
 import { Block, Checkbox, Text, Button as GaButton, theme } from 'galio-framework';
 
-
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import {View, Image } from "react-native";
-
-
 import { Button, Icon, Input } from '../components';
 import { Images, nowTheme } from '../constants';
 
@@ -28,38 +22,9 @@ const DismissKeyboard = ({ children }) => (
 
 class Ingredient extends React.Component {
   
-  constructor(props) {
-    super(props);
-    this.state = {
-     hasCameraPermission: null,
-     image: null,
-    }
-   }
-
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    this.setState({ hasCameraPermission: status === "granted" });
-  }
-  
-  _getPhotoLibrary = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-     allowsEditing: true,
-     aspect: [4, 3]
-    });
-    if (!result.cancelled) {
-     this.setState({ image: result.uri });
-    }
-   }
    
   render() {
-    const { image, hasCameraPermission } = this.state;
-  if (hasCameraPermission === null) {
-   return <View />
-  }
-  else if (hasCameraPermission === false) {
-   return <Text>Access to camera has been denied.</Text>;
-  }
-  else {
+    
     return (
       <DismissKeyboard>
         <Block flex middle>
@@ -80,99 +45,14 @@ class Ingredient extends React.Component {
                       color="#333"
                       size={18}
                     >
-                      재료 검색
+                      재료
                     </Text>
                   </Block>
 
                   <Block flex={1} middle space="between">
                     <Block center flex={0.9}>
                       <Block flex space="between">
-                        <Block>
-                            <View style={{ flex: 1 }}>
-                              <View style={styles.activeImageContainer}>
-                                {image ? (
-                                <Image source={{ uri: image }} style={{ flex: 1 }} />
-                                ) : (
-                                <View />
-                                )}
-                              </View>
-                              <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                              <Button 
-                                onPress={this._getPhotoLibrary.bind(this)} 
-                                title="Photo Picker Screen!"
-                              >
-                                <Text>사진 등록</Text>
-                              </Button>
-                              </View>
-                            </View>
-
-
-
-                          <Block width={width * 0.8} style={{ marginBottom: 5 }}>
-                           
-                          </Block>
-
-
-
-
-
-
-                          <Block>
-                            <Text
-                              style={{
-                                color: '#2c2c2c',
-                                fontWeight: 'bold',
-                                fontSize: 19,
-                                fontFamily: 'montserrat-bold',
-                                marginTop: 10,
-                                marginBottom: 10,
-                                zIndex: 2,
-                              }}
-                            >
-                              재료
-                            </Text>
-
-                            <Block row>
-                              <Button
-                                style={{ width: '42%', height: 44, marginHorizontal: 10, elevation: 0 }}
-                                textStyle={{ fontSize: 15, color: 'white' }}
-                                color="Primary"
-                                round
-                              >
-                                하늘
-                              </Button>
-                              <Button
-                                right
-                                style={{ width: '42%', height: 44, marginHorizontal: 20, elevation: 0 }}
-                                textStyle={{ fontSize: 15, color: 'white' }}
-                                color="Primary"
-                                round
-                              >
-                                부히
-                              </Button>
-                            </Block>
-                            <Block row>
-                              <Button
-                                style={{ width: '42%', height: 44, marginHorizontal: 10, elevation: 0 }}
-                                textStyle={{ fontSize: 15, color: 'white' }}
-                                color="Primary"
-                                round
-                              >
-                                지현
-                              </Button>
-                              <Button
-                                right
-                                style={{ width: '42%', height: 44, marginHorizontal: 20, elevation: 0 }}
-                                textStyle={{ fontSize: 15, color: 'white' }}
-                                color="Primary"
-                                round
-                              >
-                                유창
-                              </Button>
-                            </Block>
-                           
-                          </Block>
-                        </Block>
+                        
                         <Block center>
                           <Button color="primary" round style={styles.createButton}>
                             <Text
@@ -194,7 +74,7 @@ class Ingredient extends React.Component {
         </Block>
       </DismissKeyboard>
     );
-                            }
+                            
   }
 }
 
@@ -251,14 +131,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginHorizontal: 10,
   },
-  activeImageContainer: {
-    flex: 1,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height / 2,
-    backgroundColor: "#eee",
-    borderBottomWidth: 0.5,
-    borderColor: "#fff"
-   },
+  
 });
 
 export default Ingredient;
