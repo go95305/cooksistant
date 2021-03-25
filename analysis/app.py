d0ec5a0c8fa9d7c92bc37c493e3ef7c59ce65de5
skyyy6 <skyyy6@naver.com>
2021-03-25 21:33:38 +0900

    Fix: delete config

--- analysis/app.py
+++ analysis/app.py
@@ -1,16 +1,9 @@
 from flask import Flask
-from config.config import DevelopmentConfig
-from utils.database import db
 from flask_restplus import Api
 
 def create_app(test_config=None) :
     app = Flask(__name__)
     app.debug = True
-    # app.config.from_object(DevelopmentConfig)
-    #
-    # db.init_app(app)
-    # with app.app_context():
-    #     db.create_all()
 
     api = Api(app, title='TEST', description='레시피 추천 기능 API')
 
