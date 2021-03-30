ba8d6ddf5977e0d5c64f55bc28d9a4ce525ba408
KimHaneul <lldkdlzb@naver.com>
2021-03-31 02:47:57 +0900

    [feat] OCR GoogleVisionApi

--- frontend/screens/STT.js
+++ frontend/screens/STT.js
@@ -1,168 +1,318 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  StatusBar,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Image,
+	ActivityIndicator,
+	Button,
+	Clipboard,
+	FlatList,
+	Image,
+	Share,
+	StyleSheet,
+	Text,
+	ScrollView,
+	View
 } from 'react-native';
-import { Block, Checkbox, Text, Button as GaButton, theme } from 'galio-framework';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import uuid from 'react-native-uuid';
+import Environment from '../config/environment';
+import firebase from '../config/firebase';
 
-import { STTButton, Button, Icon, Input } from '../components';
-import { Images, nowTheme } from '../constants';
+export default class App extends React.Component {
+	state = {
+		image: null,
+		uploading: false,
+		googleResponse: null
+	};
 
-import OCR from './ocrtest';
+	async componentDidMount() {
+		await Permissions.askAsync(Permissions.CAMERA_ROLL);
+		await Permissions.askAsync(Permissions.CAMERA);
+	}
 
-const { width, height } = Dimensions.get('screen');
+	render() {
+		let { image } = this.state;
 
-const DismissKeyboard = ({ children }) => (
-  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
-);
+		return (
+			<View style={styles.container}>
+				<ScrollView
+					style={styles.container}
+					contentContainerStyle={styles.contentContainer}
+				>
+					
 
+					<View style={styles.helpContainer}>
+						<Button
+							onPress={this._pickImage}
+							title="갤러리에서 영수증등록"
+						/>
 
-class STT extends React.Component {
-    
-  render() {
-    return (
-      <DismissKeyboard>
-        <Block flex middle>
-          <ImageBackground
-            source={Images.RegisterBackground}
-            style={styles.imageBackgroundContainer}
-            imageStyle={styles.imageBackground}
-          >
-            <OCR></OCR>
-            <Block flex middle>
-              <Block style={styles.registerContainer}>
-                <Block flex space="evenly">
-                  <Block flex={0.2} middle>
-                    <Text
-                      style={{
-                        fontFamily: 'montserrat-regular',
-                        textAlign: 'center',
-                      }}
-                      color="#333"
-                      size={25}
-                      bold
-                    >
-                      가지 볶음
-                    </Text>
-                  </Block>
+						<Button onPress={this._takePhoto} title="카메라로 영수증등록" />
+						{this.state.googleResponse && (
+							<FlatList
+								data={this.state.googleResponse.responses[0].labelAnnotations}
+								extraData={this.state}
+								keyExtractor={this._keyExtractor}
+								renderItem={({ item }) => <Text>Item: {item.description}</Text>}
+							/>
+						)}
+						{this._maybeRenderImage()}
+						{this._maybeRenderUploadingOverlay()}
+					</View>
+				</ScrollView>
+			</View>
+		);
+	}
 
-                  <Block flex={1} middle space="between">
-                    <Block center flex={0.9}>
-                      <Block flex space="between">
-                        <Block middle>
-                          <Image style={styles.photo} source={Images.eggplant} />
-                        </Block>
-                        <Block flex={0.2} middle>
-                          <Text
-                            style={{
-                              fontFamily: 'montserrat-regular',
-                              textAlign: 'center',
-                            }}
-                            color="#333"
-                            size={18}
-                          >
-                            같이 요리해볼까요?
-                          </Text>
-                        </Block>
-                        
-                        <Block color="primary" round style={styles.createButton}>
-                            
-                        </Block>
-                        
-                        <Block center>
-                          <Button color="primary" round style={styles.createButton}>
-                            <Text
-                              style={{ fontFamily: 'montserrat-bold' }}
-                              size={14}
-                              color={nowTheme.COLORS.WHITE}
-                            >
-                              START
-                            </Text>
-                          </Button>
-                        </Block>
-                      </Block>
-                    </Block>
-                  </Block>
-                </Block>
-              </Block>
-              
-            </Block>
-          </ImageBackground>
-        </Block>
-      </DismissKeyboard>
-    );
-  }
+	organize = array => {
+		return array.map(function(item, i) {
+			return (
+				<View key={i}>
+					<Text>{item}</Text>
+				</View>
+			);
+		});
+	};
+
+	_maybeRenderUploadingOverlay = () => {
+		if (this.state.uploading) {
+			return (
+				<View
+					style={[
+						StyleSheet.absoluteFill,
+						{
+							backgroundColor: 'rgba(0,0,0,0.4)',
+							alignItems: 'center',
+							justifyContent: 'center'
+						}
+					]}
+				>
+					<ActivityIndicator color="#fff" animating size="large" />
+				</View>
+			);
+		}
+	};
+
+	_maybeRenderImage = () => {
+		let { image, googleResponse } = this.state;
+		if (!image) {
+			return;
+		}
+
+		return (
+			<View
+				style={{
+					marginTop: 20,
+					width: 250,
+					borderRadius: 3,
+					elevation: 2
+				}}
+			>
+				<Button
+					style={{ marginBottom: 10 }}
+					onPress={() => this.submitToGoogle()}
+					title="광학 문자 추출!"
+				/>
+
+				<View
+					style={{
+						borderTopRightRadius: 3,
+						borderTopLeftRadius: 3,
+						shadowColor: 'rgba(0,0,0,1)',
+						shadowOpacity: 0.2,
+						shadowOffset: { width: 4, height: 4 },
+						shadowRadius: 5,
+						overflow: 'hidden'
+					}}
+				>
+					<Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+				</View>
+				<Text
+					onPress={this._copyToClipboard}
+					onLongPress={this._share}
+					style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+				/>
+
+				<Text>Raw JSON:</Text>
+
+				{googleResponse && (
+					<Text
+						onPress={this._copyToClipboard}
+						onLongPress={this._share}
+						style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+					>
+						JSON.stringify(googleResponse.responses)}
+					</Text>
+				)}
+			</View>
+		);
+	};
+
+	_keyExtractor = (item, index) => item.id;
+
+	_renderItem = item => {
+		<Text>response: {JSON.stringify(item)}</Text>;
+	};
+
+	_share = () => {
+		Share.share({
+			message: JSON.stringify(this.state.googleResponse.responses),
+			title: 'Check it out',
+			url: this.state.image
+		});
+	};
+
+	_copyToClipboard = () => {
+		Clipboard.setString(this.state.image);
+		alert('Copied to clipboard');
+	};
+
+	_takePhoto = async () => {
+		let pickerResult = await ImagePicker.launchCameraAsync({
+			allowsEditing: true,
+			aspect: [4, 3]
+		});
+
+		this._handleImagePicked(pickerResult);
+	};
+
+	_pickImage = async () => {
+		let pickerResult = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			aspect: [4, 3]
+		});
+
+		this._handleImagePicked(pickerResult);
+	};
+
+	_handleImagePicked = async pickerResult => {
+		try {
+			this.setState({ uploading: true });
+
+			if (!pickerResult.cancelled) {
+				//uploadUrl = await uploadImageAsync(pickerResult.uri);
+				//this.setState({ image: uploadUrl });
+        this.setState({ image: pickerResult.uri });
+			}
+		} catch (e) {
+			console.log(e);
+			alert('Upload failed, sorry :(');
+		} finally {
+			this.setState({ uploading: false });
+		}
+	};
+
+	submitToGoogle = async () => {
+		try {
+			this.setState({ uploading: true });
+			let { image } = this.state;
+			let body = JSON.stringify({
+				requests: [
+					{
+						features: [
+							{ type: 'LABEL_DETECTION', maxResults: 10 },
+							{ type: 'LANDMARK_DETECTION', maxResults: 5 },
+							{ type: 'FACE_DETECTION', maxResults: 5 },
+							{ type: 'LOGO_DETECTION', maxResults: 5 },
+							{ type: 'TEXT_DETECTION', maxResults: 5 },
+							{ type: 'DOCUMENT_TEXT_DETECTION', maxResults: 5 },
+							{ type: 'SAFE_SEARCH_DETECTION', maxResults: 5 },
+							{ type: 'IMAGE_PROPERTIES', maxResults: 5 },
+							{ type: 'CROP_HINTS', maxResults: 5 },
+							{ type: 'WEB_DETECTION', maxResults: 5 }
+						],
+						image: {
+							source: {
+								imageUri: image
+							}
+						}
+					}
+				]
+			});
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+			let response = await fetch(
+				'https://vision.googleapis.com/v1/images:annotate?key=' +
+					Environment['GOOGLE_CLOUD_VISION_API_KEY'],
+				{
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json'
+					},
+					method: 'POST',
+					body: body
+				}
+			);
+			let responseJson = await response.json();
+      console.log('###########################################')
+			console.log(responseJson);
+      console.log('###########################################')
+
+			this.setState({
+				googleResponse: responseJson,
+				uploading: false
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+}
+
+async function uploadImageAsync(uri) {
+	const blob = await new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.onload = function() {
+			resolve(xhr.response);
+		};
+		xhr.onerror = function(e) {
+			console.log(e);
+			reject(new TypeError('Network request failed'));
+		};
+		xhr.responseType = 'blob';
+		xhr.open('GET', uri, true);
+		xhr.send(null);
+	});
+
+	const ref = firebase
+		.storage()
+		.ref()
+		.child(uuid.v4());
+	const snapshot = await ref.put(blob);
+
+	blob.close();
+
+	return await snapshot.ref.getDownloadURL();
 }
 
 const styles = StyleSheet.create({
-  imageBackgroundContainer: {
-    width: width,
-    height: height,
-    padding: 0,
-    zIndex: 1,
-  },
-  imageBackground: {
-    width: width,
-    height: height,
-  },
-  registerContainer: {
-    marginTop: 55,
-    width: width * 0.9,
-    height: height < 812 ? height * 0.8 : height * 0.8,
-    backgroundColor: nowTheme.COLORS.WHITE,
-    borderRadius: 4,
-    shadowColor: nowTheme.COLORS.BLACK,
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowRadius: 8,
-    shadowOpacity: 0.1,
-    elevation: 1,
-    overflow: 'hidden',
-  },
-  inputIcons: {
-    marginRight: 12,
-    color: nowTheme.COLORS.ICON_INPUT,
-  },
-  inputs: {
-    borderWidth: 1,
-    borderColor: '#E3E3E3',
-    borderRadius: 21.5,
-  },
-  passwordCheck: {
-    paddingLeft: 2,
-    paddingTop: 6,
-    paddingBottom: 15,
-  },
-  createButton: {
-    width: width * 0.5,
-    marginTop: 25,
-    marginBottom: 30,
-  },
-  social: {
-    width: theme.SIZES.BASE * 3.5,
-    height: theme.SIZES.BASE * 3.5,
-    borderRadius: theme.SIZES.BASE * 1.75,
-    justifyContent: 'center',
-    marginHorizontal: 10,
-  },
-  photo: {
-    borderRadius: 20,
-    height: 230,
-    width: 300,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		paddingBottom: 10
+	},
+	developmentModeText: {
+		marginBottom: 20,
+		color: 'rgba(0,0,0,0.4)',
+		fontSize: 14,
+		lineHeight: 19,
+		textAlign: 'center'
+	},
+	contentContainer: {
+		paddingTop: 30
+	},
 
-export default STT;
+	getStartedContainer: {
+		alignItems: 'center',
+		marginHorizontal: 50
+	},
+
+	getStartedText: {
+		fontSize: 17,
+		color: 'rgba(96,100,109, 1)',
+		lineHeight: 24,
+		textAlign: 'center'
+	},
+
+	helpContainer: {
+		marginTop: 15,
+		alignItems: 'center'
+	}
+});
