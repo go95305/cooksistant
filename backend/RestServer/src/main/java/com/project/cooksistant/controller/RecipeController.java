ed799047e27570964570db6bdf8ae585f8e726e7
KYC <go95305@naver.com>
2021-04-03 15:00:12 +0900

    Update recipeController change parameter name in mainImage method

--- backend/RestServer/src/main/java/com/project/cooksistant/controller/RecipeController.java
+++ backend/RestServer/src/main/java/com/project/cooksistant/controller/RecipeController.java
@@ -49,7 +49,7 @@ public class RecipeController {
             "                                                   - file: 사진\n" +
             "                                                   - recipeId: 레시피 인덱스")
     @PostMapping(value = "/recipe/mainImage")
-    public void mainImage(@RequestParam String originalName, @RequestParam("file") MultipartFile file, @RequestParam("recipeId") Long recipeId) throws IOException {
+    public void mainImage(@RequestParam String originalName, @RequestParam MultipartFile file, @RequestParam("recipeId") Long recipeId) throws IOException {
         System.out.println(file.getOriginalFilename()+"=======================");
         recipeService.mainImage(originalName, file, recipeId);
     }
